51ee299ddcc8b7a4c29fd3d531150fee31c19c84
Ustad Nordin <bouchtaoui.dev@gmail.com>
2024-03-25 18:56:14 +0100

    implemented remove function

--- proto_list.c
+++ proto_list.c
@@ -23,3 +23,19 @@ int __get_element(listbuf_t* lb, int index, void* element) {
 
     return -1;
 }
+
+int __remove_element(listbuf_t* lb, int index, void* element) {
+    if (lb->index >= 0 && index < lb->index) {
+        memcpy(element, lb->buffer+(index*lb->element_size), lb->element_size);
+
+        int count = lb->index - index - 1;
+        for (size_t i = 0; i < count; i++) {
+            memcpy(lb->buffer+(index*lb->element_size), lb->buffer+((index+i+1)*lb->element_size), lb->element_size);
+        }
+        
+        
+        return 0;
+    } 
+
+    return -1;
+}
